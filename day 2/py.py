224d03703dac344833c7b5a8f30da1e2a81c8baf
mohammed k <msm.kasid@gmail.com>
2025-10-14 09:35:30 -0600

    hdfj

---
+++ day 2/py.py
@@ -0,0 +1,14 @@
+operator = input("What do you wanna do? +, -, *, / ")
+num1 = int(input("First number? "))
+num2 = int(input("Second number? "))
+
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1 - num2)
+elif operator == "*":
+    print(num1 * num2)
+elif operator == "/":
+    print (num1 / num2)
+else:
+    print('fatty u dum as hell')
--- day 6/day-6.py
+++ day 6/day-6.py
@@ -1,1 +1,44 @@
-# while loops, cume quiz
+# while loops, recap quiz
+
+# quiz
+
+# while loops - infinite loops, foundation of apps that allow the user to run it
+# multiple times
+
+while True:
+    print("hi")
+    break
+
+# most cases will use True or something similar
+
+# how to end it
+# SENTINEL
+
+sentinel = "stop"
+
+while True:
+    name = input("What is your name? ")
+    if name == sentinel:
+        break
+
+    print("Hello", name)
+
+
+# use functions for repeatability
+
+def collect_names():
+    sentinel = "end"
+    names = []
+    while True:
+        name = input("Enter a name (or 'end' to stop): ")
+        if name == sentinel:
+            break
+        names.append(name)
+    return names
+
+print("Names:", collect_names())
+
+# TEACH += etc
+
+# quiz
+#activity: calculator
