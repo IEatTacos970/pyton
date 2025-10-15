a4168e6d005ac84f8dad60a439e62167b3051254
mohammed k <msm.kasid@gmail.com>
2025-10-15 10:34:57 -0600

    jsdf

--- day 7/day-7.py
+++ day 7/day-7.py
@@ -1,1 +1,21 @@
 # intro to planning, ceasar cypher guided project
+
+# go over calculator solution, explain it very thorouly
+
+
+# lucidchart flowcharting
+
+# teach how to use site
+
+# activity: flowchart a simple procedure of how you can go to the store and get bread
+
+
+# ceasar cypher project together. first chart then code.
+
+
+
+
+# tensorflow pytorch -- ai
+# pygame -- game dev
+# numpy -- everything math
+# beautifl soup -- web scraping
